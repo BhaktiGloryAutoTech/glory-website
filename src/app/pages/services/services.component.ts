9ea2ab1ba9362f27ff1babed099dc1e2aa06c184
Bhakti Madia <bhaktimadia@github.com>
2021-01-06 18:56:13 +0530

    service api bind

--- src/app/pages/services/services.component.ts
+++ src/app/pages/services/services.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { ServiceService } from "src/app/@theme/services/service.service";
 
 @Component({
-  selector: 'app-services',
-  templateUrl: './services.component.html',
-  styleUrls: ['./services.component.css']
+  selector: "app-services",
+  templateUrl: "./services.component.html",
+  styleUrls: ["./services.component.css"],
 })
 export class ServicesComponent implements OnInit {
-
-  constructor() { }
-
+  constructor(private serviceService: ServiceService) {}
+  serviceList: any = [];
   ngOnInit(): void {
+    this.getServiceList();
   }
 
+  getServiceList() {
+    this.serviceService.getServiceList().subscribe(
+      (data: any) => {
+        this.serviceList = data["data"];
+        console.log(this.serviceList);
+      },
+      (error) => {}
+    );
+  }
 }
