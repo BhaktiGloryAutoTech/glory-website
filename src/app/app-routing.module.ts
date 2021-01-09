7eb25c470a8fc3db1a3be63b7e757a5b51f378a0
Bhakti Madia <bhaktimadia@github.com>
2021-01-09 12:34:58 +0530

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,19 +1,33 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
+import { AboutComponent } from "./pages/about/about.component";
+import { ContactComponent } from "./pages/contact/contact.component";
+import { HomeComponent } from "./pages/home/home.component";
+import { NeedTeamComponent } from "./pages/need-team/need-team.component";
+import { ServicesComponent } from "./pages/services/services.component";
+import { WorkComponent } from "./pages/work/work.component";
 
 const routes: Routes = [
   {
     path: "pages",
-    loadChildren: () =>
-      import("./pages/pages.module").then((m) => m.PagesModule),
-    //     children:[
-    //       {
-    //   path: "career",
-    //   loadChildren: () =>
-    //     import("./pages/career/career.module").then((m) => m.CareerModule),
-    // },
 
-    //     ]
+    children: [
+      { path: "", redirectTo: "home", pathMatch: "full" },
+      {
+        path: "home",
+        component: HomeComponent,
+      },
+      { path: "contact", component: ContactComponent },
+      { path: "about", component: AboutComponent },
+      { path: "needTeam", component: NeedTeamComponent },
+      { path: "services", component: ServicesComponent },
+      { path: "work", component: WorkComponent },
+      {
+        path: "career",
+        loadChildren: () =>
+          import("./pages/career/career.module").then((m) => m.CareerModule),
+      },
+    ],
   },
   { path: "", redirectTo: "pages", pathMatch: "full" },
 
