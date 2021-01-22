bf4d9a47de76884d64d1f7705a6dbe232f0f25fd
Bhautik <bhautik.autotech@gmail.com>
2021-01-22 18:32:01 +0530

    home page slider changes, work page slider changes

--- src/app/pages/portfolio/portfolio.component.ts
+++ src/app/pages/portfolio/portfolio.component.ts
@@ -13,10 +13,36 @@ export class PortfolioComponent implements OnInit {
   slideConfig = {
     slidesToShow: 4,
     slidesToScroll: 1,
-    nextArrow: "<div class='nav-btn next-slide'></div>",
-    prevArrow: "<div class='nav-btn prev-slide'></div>",
+    prevArrow:'<button type="button" class="slick-prev"></button>',
+  nextArrow:'<button type="button" class="slick-next"></button>',
     dots: true,
     infinite: false,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 767,
+        settings: {
+          slidesToShow: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1
+        }
+      }
+    ]
   };
   constructor(private workService: WorkService, private route: Router) {}
 
