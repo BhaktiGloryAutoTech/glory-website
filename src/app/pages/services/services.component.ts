b5c28cf3330cc6cab5d6cca1b1c5372e4967d8e5
Bhakti Madia <bhaktimadia@github.com>
2021-02-12 16:28:39 +0530

    dynamic Contact No and dynamic Pop up

--- src/app/pages/services/services.component.ts
+++ src/app/pages/services/services.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit, TemplateRef } from "@angular/core";
 import { ServiceService } from "src/app/@theme/services/service.service";
 import { ElementRef, ViewChild } from "@angular/core";
 import { NgbModal } from "@ng-bootstrap/ng-bootstrap";
+import { FormControl, FormGroup, Validators } from "@angular/forms";
+import { ContactService } from "src/app/@theme/services/contact.service";
+import { ToastrService } from "ngx-toastr";
 @Component({
   selector: "app-services",
   templateUrl: "./services.component.html",
@@ -10,17 +13,26 @@ import { NgbModal } from "@ng-bootstrap/ng-bootstrap";
 export class ServicesComponent implements OnInit {
   @ViewChild("content", { static: true })
   content!: TemplateRef<any>;
+  @ViewChild("success", { static: true })
+  success!: TemplateRef<any>;
   mod: any;
+  public loading = false;
+  contactForm: FormGroup | any;
+  formSubmitted: boolean = false;
   constructor(
     private serviceService: ServiceService,
-    private modalService: NgbModal
+    private modalService: NgbModal,
+    private contactService: ContactService,
+    private tostrService: ToastrService
   ) {}
   serviceList: any = [];
   ngOnInit(): void {
     window.scroll(0, 0);
-    this.getServiceList();
+    //this.getServiceList();
+    this.modalService.open(this.success, { centered: true });
+    this.getContactData();
     this.mod = setInterval(() => {
-      this.modalService.open(this.content,{centered:true});
+      this.modalService.open(this.content, { centered: true });
       clearInterval(this.mod);
     }, 7000);
   }
@@ -32,5 +44,37 @@ export class ServicesComponent implements OnInit {
       },
       (error) => {}
     );
+  }
+  getContactData() {
+    this.contactForm = new FormGroup({
+      Name: new FormControl(null, Validators.required),
+      Email: new FormControl(null, [
+        Validators.required,
+        Validators.pattern(
+          /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+\.[a-zA-Z]{2,}/
+        ),
+      ]),
+      ContactNo: new FormControl(null, [
+        Validators.required,
+        Validators.pattern(/^(?:(?:\+|0{0,2})91(\s*[\-]\s*)?|[0]?)?[789]\d{9}/),
+      ]),
+    });
+  }
+  addContact() {
+    this.formSubmitted = true;
+    this.loading = true;
+    if (this.contactForm.valid) {
+      this.contactService.getInTouch(this.contactForm.value).subscribe(
+        (data) => {
+          this.loading = false;
+          this.modalService.dismissAll();
+          this.tostrService.success("WE Will Get Back to you soon");
+        },
+        (error) => {}
+      );
+    } else {
+      this.loading = false;
+      return;
+    }
   }
 }
